87a919df17139c7f2ec35e665e3ee6970f13164c
Antoine Latrille <antoine.latrille@gmail.com>
2020-11-29 19:03:03 +0100

    Add a fully working simple echo websocket server

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use actix::{Actor, StreamHandler};
+use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Error};
+use actix_web_actors::ws;
 
-async fn index(_req: HttpRequest) -> impl Responder {
-    "Hello."
+
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
 }
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        println!("{:?}", msg);
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) if text == "ping" => ctx.text(String::from("pong")),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("{:?}\n", resp);
+    resp
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // load ssl keys
-    // to create a self-signed temporary cert for testing:
-    // `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file("key.pem", SslFiletype::PEM)
-        .unwrap();
-    builder.set_certificate_chain_file("cert.pem").unwrap();
-
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind_openssl("127.0.0.1:8080", builder)?
+    println!("Starting server");
+    let res = HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
+        .bind("127.0.0.1:8080")?
         .run()
-        .await
+        .await;
+    println!("Bye!");
+    res
 }
